b4febe78e73c02a0bb2690e667d2bd3746beb99a
dylan <dylan001@Dylans-MacBook-Pro.local>
2023-07-31 22:25:03 +0800

    test

---
+++ __tests__/test.spec.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react'
+import DigitalClock from '../src/react-digital-clock'
+
+// test('test react-digital-clock with `className`.', () => {
+//     const component = mount(<DigitalClock className="my-component" />)
+//     expect(component.find('time').length).toBe(1)
+//     expect(component.find('time').prop('className')).toEqual('my_classname')
+// })
+//
+// test('test react-digital-clock with `isEastTime`.', () => {
+//     let d = +new Date() - 3601 * 1000; // an hour ago
+//     let component = mount(<DigitalClock isEastTime={true} />)
+//     expect(component.find('time').length).toBe(1);
+// });
